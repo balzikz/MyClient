519682fbabe2e2328eb0783da7e0a543c841edfd
balzikz <alikkhhhannnn@gmail.com>
2026-06-24 23:28:27 +0500

    Add native bootstrap activity

---
+++ app/src/main/java/com/balzikz/mathclient/BootstrapActivity.java
@@ -0,0 +1,24 @@
+package com.balzikz.mathclient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public final class BootstrapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String message = NativeBridge.isLoaded()
+                ? "MATH Native Core: ONLINE"
+                : "MATH Native Core: FAILED";
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+}
